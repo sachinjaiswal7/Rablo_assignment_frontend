bf43183c56cb3ce169532cfa26f5d948573d7411
sachin <sj62513@gmail.com>
2023-08-12 11:03:08 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {  Route, Routes, useNavigate } from "react-router-dom"
 import axios from "axios";
 import './App.scss';
